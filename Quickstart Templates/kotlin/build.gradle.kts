66693a7c9a36a23adc4c611f546a9156e620c869
Justin Man <justin.man@moonpig.com>
2020-02-12 15:25:27 +0000

    Refactor example test to use imported libraries

--- Quickstart Templates/kotlin/build.gradle.kts
+++ Quickstart Templates/kotlin/build.gradle.kts
@@ -11,6 +11,9 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
+
+    testImplementation("junit:junit:4.13")
+    testImplementation("org.assertj:assertj-core:3.15.0")
 }
 
 tasks {
--- Quickstart Templates/kotlin/src/test/kotlin/ExampleTest.kt
+++ Quickstart Templates/kotlin/src/test/kotlin/ExampleTest.kt
@@ -1,7 +1,12 @@
+import org.junit.Test
+import org.assertj.core.api.Assertions.assertThat
+
 class ExampleTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     internal fun shouldAddTwoNumbers() {
+        val result = add(2, 3)
 
+        assertThat(result).isEqualTo(5)
     }
 }
