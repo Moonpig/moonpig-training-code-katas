c6bb4582bc046fd275c06d14bd76b022963750dd
Steve Phillips <steve.phillips@moonpig.com>
2021-01-14 10:42:04 +0000

    Added GildedRose kata

---
+++ Katas/GildedRose/dotnetcore/GildedRose.Tests/ApprovalTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Xunit;
+
+namespace GildedRose.Tests
+{
+    [UseReporter(typeof(DiffReporter))]
+    public class ApprovalTest
+    {
+        [Fact]
+        public void ThirtyDays()
+        {
+            var fakeoutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeoutput));
+            Console.SetIn(new StringReader("a\n"));
+
+            Program.Main(new string[] { });
+            var output = fakeoutput.ToString();
+
+            Approvals.Verify(output);
+        }
+    }
+}
